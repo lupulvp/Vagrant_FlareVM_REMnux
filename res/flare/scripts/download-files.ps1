76fb17148de6a8b2601b8ed52ae24bec399aa512
lupulvp <lupu.lvp@gmail.com>
2024-06-27 11:06:14 +0300

    make file download quicker

--- res/flare/scripts/download-files.ps1
+++ res/flare/scripts/download-files.ps1
@@ -1,4 +1,11 @@
 param ([String] $file_url, [String] $file_path)
 
 Write-Host "Downloading $file_url to $file_path"
-Invoke-WebRequest -Uri $file_url -OutFile $file_path
+
+# # this method is very slow
+# Invoke-WebRequest -Uri $file_url -OutFile $file_path
+
+# # this method is faster
+(New-Object System.Net.WebClient).DownloadFile($file_url, $file_path)
+
+Write-Host "Downloaded $file_url to $file_path"
